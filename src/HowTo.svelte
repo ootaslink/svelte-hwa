cfc70efc60cfa04ada01967577881ee10d0a7680
ootaslink <ootaslink@gmail.com>
2023-11-24 16:57:55 +0300

    committing html

---
+++ src/HowTo.svelte
@@ -0,0 +1,13 @@
+<div bp="grid">
+  <img
+    bp="offset-5@md 4@md 12@sm"
+    src="handwashing.gif"
+    alt="How to wash your hands."
+  />
+</div>
+
+<style>
+  img {
+    width: 100%;
+  }
+</style>
---
+++ src/ProgressBar.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let progress = 0;
+</script>
+
+<div bp="grid">
+  <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+    <div class="progress-bar" style="width: {progress}%;">
+      <span class="sr-only">%{progress}</span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .progress-container {
+    margin: 20px 0;
+    border: solid rgb(194, 194, 194) 1px;
+  }
+  .progress-bar {
+    height: 30px;
+    background-color: rgb(77, 199, 55);
+  }
+</style>
